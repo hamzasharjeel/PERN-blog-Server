2d0e74a7fbbf5cab2d4337e4a91fcd6737bb8e04
hamzasharjeel <hamzasharjeel8@gmail.com>
2023-09-02 13:46:42 -0700

    dsds

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+const express = require('express');
 const client_1 = require("@prisma/client");
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const bcrypt_1 = __importDefault(require("bcrypt"));
